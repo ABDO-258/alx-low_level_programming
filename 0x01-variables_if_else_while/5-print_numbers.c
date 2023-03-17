ef27fe38bc4920531b1d6a9e597ccdbc56951532
karbalabdo <abdoraja2005@gmail.com>
2023-03-17 14:25:08 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main -Entry point
+ *
+ * Return: must be (0)
+ */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int ch;
+
+	for (ch = 0 ; ch < 10 ; ch++)
+	{
+	putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
