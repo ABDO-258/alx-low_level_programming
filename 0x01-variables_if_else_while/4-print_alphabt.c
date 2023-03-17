86e0751c1c534f0b111ca8d8b57596f16ecbfcdf
karbalabdo <abdoraja2005@gmail.com>
2023-03-17 14:15:14 -0700

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main -Entry point
+ *
+ * Return: must be (0)
+ */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z'; ch++)
+	{
+		if (ch == 'e' || ch == 'q')
+		{
+			ch++;
+		}
+	putchar(ch);
+	putchar('\n');
+	return (0);
+}
