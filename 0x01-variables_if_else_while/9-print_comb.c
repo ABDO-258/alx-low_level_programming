539cc96323630d7b7bd0dc808b6946a1e51aeec4
karbalabdo <abdoraja2005@gmail.com>
2023-03-17 15:55:16 -0700

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ *
+ * Return: must be (0)
+ */
+
+int main(void)
+{
+	int ch,te = 9;
+
+	for (ch = 0 ; ch <= te ; ch++)
+	{
+		/*change number to ascii */
+	putchar(ch + '0');
+	if (ch != 9)
+		{
+		putchar(',');
+		putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
