e39acb538fece270c8d052ebc7cbae1f606d40ec
karbalabdo <abdoraja2005@gmail.com>
2023-03-21 12:55:20 -0700

    task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *_isalpha - Alphabet a-z
+ *
+ *@c: input of function
+ *
+ * Return: 1 or 0
+ */
+int _isalpha(int c)
+{
+	if (c >= 97 && c <= 122 && c >= 65 && c <= 90)
+		return (1);
+	return (0);
+}
