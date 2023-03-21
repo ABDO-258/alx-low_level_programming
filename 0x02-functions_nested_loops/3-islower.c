291efcc870ca07ae7112d4306ce1bafb36256cf6
karbalabdo <abdoraja2005@gmail.com>
2023-03-21 11:57:01 -0700

    task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet_x10 - print alphabet a-z 10 time
+ *
+ * Return: no return
+ */
+int _islower(int c);
+{
+	if (c >= 97 && c <= 122)
+		return(1);
+	return(0);
+}
