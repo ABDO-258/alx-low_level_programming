a5d8467ed06ecf3fc35febae4da15e09ef43c007
karbalabdo <abdoraja2005@gmail.com>
2023-03-21 07:36:42 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+*main -Entry point
+*
+*Return: must return 0
+*/
+int main(void)
+{
+	char text[] = "_putchar";
+	int ch;
+
+	for (ch = 0; ch < 8 ; ch++)
+		_putchar(text[ch]);
+	_putchar("\n");
+	return (0);
+}
