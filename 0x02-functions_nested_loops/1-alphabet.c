d7d7b00283e57cef77b5a93b8b252da37e090249
karbalabdo <abdoraja2005@gmail.com>
2023-03-21 08:54:55 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.c"
+/**
+ * print_alphabet - print alphabet a-z
+ *
+ * Return: no return
+ */
+void print_alphabet(void)
+{
+	char alphabet[] ="abcdefghijklmnopqrstuvwxyz";
+	int ch;
+
+	for (ch = 0; ch < 26; ch++)
+		_putchar(alphabet[ch]);
+	_putchar('\n');
+	
+}
