b35cbdbec6b4e160e2120a8044a2cedff348266f
karbalabdo <abdoraja2005@gmail.com>
2023-03-21 17:23:30 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *_abs - calculer the absolute number
+ *
+ *@n: input of function
+ *
+ * Return: n
+ */
+int _abs(int n);
+{
+	if (n < 0)
+		n = (-1) * n;
+	return (0);
+}
