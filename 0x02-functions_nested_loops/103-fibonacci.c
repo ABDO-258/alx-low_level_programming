f9102a3f417a71e136da4b5b5d0a06f50b6eebad
karbalabdo <abdoraja2005@gmail.com>
2023-03-22 16:18:21 -0700

    task 15

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -6,14 +6,16 @@
  */
 int main(void)
 {
-	unsigned long  sum, fib1 = 0, fib2 = 1, sum_of_even;
+	unsigned long  sum, fib1 = 0, fib2 = 1, sum_of_even = 0;
 
 	do {
 		sum = fib1 + fib2;
 		fib1 = fib2;
 		fib2 = sum;
 		if ((sum % 2) == 0)
+		{
 		sum_of_even += sum;
+		}
 	} while (sum < 4000000);
 		printf("%lu", sum_of_even);
 	return (0);
