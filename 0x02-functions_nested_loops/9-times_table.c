7b2c50fe2005d948eea93f65325ed1a7b4e00cf5
karbalabdo <abdoraja2005@gmail.com>
2023-03-22 07:10:58 -0700

    task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *times_table - print 9 times table
+ *
+ *
+ *
+ *
+ *
+ */
+void times_table(void)
+{
+	int i, j, k;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			k = i * j
+			if (j == 0)
+			{
+				_putchar(('0');
+			}
+			else if (k <= 9)
+			{
+				_putchar(' ');
+				_putchar((k + '0');
+			}
+			else
+			{
+				_putchar(k / 10 + '0');
+				_putchar(k % 10 + '0');
+			}
+			if (j != 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
