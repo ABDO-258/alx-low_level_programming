2e7432ca1a0cf4a3a2497c848c614fb5c0b834bd
karbalabdo <abdoraja2005@gmail.com>
2023-03-22 13:05:10 -0700

    task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -3,10 +3,7 @@
 /**
  *print_times_table - print n times table
  *
- *
  *@n:number
- *
- *
  */
 void print_times_table(int n)
 {
@@ -14,17 +11,13 @@ void print_times_table(int n)
 
 	if (n < 15 && n >= 0)
 	{
-
 	for (i = 0; i <= n; i++)
 	{
 		for (j = 0; j <= n; j++)
-		{
-			k = i * j;
+		{	k = i * j;
 
 			if (j == 0)
-			{
 				_putchar('0');
-			}
 			else if (k <= 9)
 			{
 				_putchar(' ');
@@ -44,8 +37,10 @@ void print_times_table(int n)
 				_putchar(k % 10 + '0');
 			}
 			if (j != n)
+			{
 				_putchar(',');
 				_putchar(' ');
+			}
 		}
 		_putchar('\n');
 	}
