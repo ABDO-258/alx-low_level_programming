52b1453198b335ab1d8f88793baf892f1802ee8d
karbalabdo <abdoraja2005@gmail.com>
2023-03-21 19:10:31 -0700

    task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,10 +12,17 @@ void print_to_98(int n)
 {
 	int count;
 	if (n > 98)
+	{
 		for (count = n; count > 98; --count)
+		{
 			printf("%d, ", count);
+		}
+	}
 	else
+	
 		for (count = n; count < 98; ++count)
 			printf("%d, ", count);
 			printf("98\n");
-}
+
+ioid jack_bauer(void)
+
