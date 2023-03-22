03656d615bdd3833746b59c708b4b2fa383acf65
karbalabdo <abdoraja2005@gmail.com>
2023-03-22 15:41:24 -0700

    task 15

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *main - Entry point sum of even fibonacci number
+ *
+ *Return: return 0
+ */
+int main(void)
+{
+	unsigned long  sum, fib1 = 0, fib2 = 1, sum_of_even;
+
+	do {
+		sum = fib1 + fib2;
+		fib1 = fib2;
+		fib2 = sum;
+		if ((sum % 2) == 0)
+		sum_of_even += sum;
+	} while (sum < 4000000);
+		printf("%lu", sum_of_even);
+	return (0);
+}
