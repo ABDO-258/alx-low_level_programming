5d80bac4cf91e5609c1e82b06526c904fe354dcb
karbalabdo <abdoraja2005@gmail.com>
2023-03-23 14:53:50 -0700

    3 print number from 1 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+/**
+ *print_numbers - print numbers from 0 to 9
+ *@n:number 
+ *Return:void
+ **/
+void print_numbers(void)
+{
+	for (n = (0 + '0') ; n < (10 + '0'); n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+	return (0);
+}
