25849ec49461a928dea599eaf2d48947760c80df
karbalabdo <abdoraja2005@gmail.com>
2023-03-22 23:48:38 -0700

    task 0 v1

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+/**
+ *
+ * _isupper - chek if char is uppercase
+ * 
+ * @c: input
+ *
+ *Return:1 if upper 0 if not
+ */
+
+int _isupper(int c)
+{
+    if (c>=65 && c<=90)
+    return(1);
+    else 
+    return(0);
+}
