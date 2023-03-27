1c8a1d5892c640f19776e2e9336178e7bec6a610
abdo <abdoraja2005@gmail.com>
2023-03-27 16:12:19 +0000

    returns the length of a string 01

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+/**
+ * _strlen - returns the length of a string
+ *
+ *@s:string
+ *
+ * Return: length of a string
+*/
+int _strlen(char *s)
+{
+	int length;
+
+	for (length = 0; *s != '\0'; s++)
+	{
+		length++;
+	}
+	return (length);
+}
