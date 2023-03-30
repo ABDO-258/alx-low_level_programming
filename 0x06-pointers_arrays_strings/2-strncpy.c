9b5f29d5c7fc1c88736770c0785a1e9e5577a9eb
abdo <abdoraja2005@gmail.com>
2023-03-30 23:33:50 +0000

    copie a string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+/**
+ * _strncpy - copie a strings with n
+ *
+ *@dest:string destination
+ *@n:number of character
+ *@src:string source
+ *
+ *Return: a string
+ *
+ */
+char *_strncpy(char *dest, char *src, int n);
+{
+	int j = 0;
+
+	while (src[j] != '\0' && j < n)
+	{
+		dest[j] = src[j]
+		j++;
+	}
+	dest[j] = '\0';
+	return (dest);
+}
