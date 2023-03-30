c20fb6889fa7314df647bfb147cd5515fc52e58f
abdo <abdoraja2005@gmail.com>
2023-03-30 04:15:56 +0000

    task01

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+/**
+ * _stnrcat - concatenates two strings with n 
+ *
+ *@dest:string destination
+ *@n:number of character 
+ *@src:string source
+ *
+ *Return:2 string in 1
+ *
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0, j = 0;
+	char *result;
+
+	result = dest;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (src[j] != src[n])
+	{
+		result[i] = src[j];
+		i++;
+		j++;
+	}
+	result[i] = '\0';
+	return (dest);
+}
