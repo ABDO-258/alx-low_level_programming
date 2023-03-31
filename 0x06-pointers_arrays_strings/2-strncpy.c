f51f3e8b2f30e5fce1015b80c211645deed1fc87
abdo <abdoraja2005@gmail.com>
2023-03-31 20:10:05 +0000

    finaly the compilation work

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,19 +12,15 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int j = 0;
 
-	while (src[j] != '\0')
+	while (src[j] != '\0' && j < n)
 	{
-		if (j < n)
-		{
-			dest[j] = src[j];
-		}
-		else 
-		{
-			dest[j] = dest[j];
-		}
-
+		dest[j] = src[j];
 		j++;
 	}
-	dest[j] = '\0';
+	while (j < n)
+	{
+		dest[j] = '\0';
+		j++;
+	}
 	return (dest);
 }
