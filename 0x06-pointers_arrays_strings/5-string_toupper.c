d196738d7feecb232e340aa5490b8127ce50fc65
abdo <abdoraja2005@gmail.com>
2023-04-03 01:42:53 +0000

    task 5 lowercase to uppercase fix betty error

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,16 +1,17 @@
 /**
  * *string_toupper - change array from lowercasse to uppercasse
  *
- *@s:pointer to array 
+ *@s:pointer to array
  *
  *Return: the pointer s
  */
 char *string_toupper(char *s)
 {
 	int i = 0;
+
 	while (s[i] != '\0')
 	{
-		if ( s[i] >= 97 && s[i] <= 122)
+		if (s[i] >= 97 && s[i] <= 122)
 		{
 			s[i] = s[i] - 32;
 		}
