1e003b1b000aea5832df4a852f9eeda09f5dccb1
abdo <abdoraja2005@gmail.com>
2023-04-03 01:40:33 +0000

    task 5 lowercase to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+/**
+ * *string_toupper - change array from lowercasse to uppercasse
+ *
+ *@s:pointer to array 
+ *
+ *Return: the pointer s
+ */
+char *string_toupper(char *s)
+{
+	int i = 0;
+	while (s[i] != '\0')
+	{
+		if ( s[i] >= 97 && s[i] <= 122)
+		{
+			s[i] = s[i] - 32;
+		}
+		i++;
+	}
+	return (s);
+}
