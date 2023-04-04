ffde4b2e942ce0371c5780720ebf6b2530804b44
abdo <abdoraja2005@gmail.com>
2023-04-04 00:02:21 +0000

    locate a character in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+/**
+ * _strchr - locate a character in a string
+ *
+ * @c:the charcter to locate
+ * @s:pointer to the first ocurrence
+ *
+ * Return: (adress of the lacted lettre or NULL if not found
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+		{
+			return (&s[i]);
+		}
+		i++;
+	}
+	return (NULL);
+}
