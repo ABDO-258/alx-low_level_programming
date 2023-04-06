4e05aaab7234de855e0b3be1024dffb69c09e255
abdo <abdoraja2005@gmail.com>
2023-04-06 05:15:00 +0000

    task 2 print length of a string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+
+#include <unistd.h>
+#include "main.h"
+/**
+ *_strlen_recursion- print length of a string in recursion
+ *
+ * @s:pointer to string
+ *
+ * Return: length of string
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+		return (1 + _strlen_recursion(s + 1));
+}
