a718fc7f7ee0ee0adf8a5fc0ea8bd31d8c247ec1
abdo <abdoraja2005@gmail.com>
2023-04-07 18:14:38 +0000

    mutiplie two argument

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - program to multiplies two number
+ *
+ *@argc: argument counter
+ *@argv: pointer to array o argument
+ *
+ *Return:  1 if error 
+ *
+ */
+int main(int argc, char *argv[])
+{
+	int result;
+
+	if (argc == 3)
+	{
+		result = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", result);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
