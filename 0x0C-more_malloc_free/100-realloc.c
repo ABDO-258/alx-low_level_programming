0c3ad631a67384b734645fbdf821a4d474e0937b
abdo <abdoraja2005@gmail.com>
2023-04-15 00:51:02 +0000

    advanced task

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -17,22 +17,21 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	char *q = (char *) new_ptr;
 	unsigned int i;
 
+	if (new_ptr == NULL)
+		return (NULL);
+	if (ptr == NULL)
+	{
+		if (new_ptr == NULL)
+			return (NULL);
+		return (new_ptr);
+	}
 	if (new_size == old_size)
 		return (ptr);
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
-	}
-	if (ptr == NULL)
-	{
-		if (new_ptr == NULL)
-			return (NULL);
-
-		return (new_ptr);
-	}
-	if (new_ptr == NULL)
-		return (NULL);
+	}	
 	if (new_size > old_size)
 	{
 	for (i = 0; i < new_size && i < old_size; i++)
