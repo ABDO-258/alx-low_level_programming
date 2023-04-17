0aef2ee111cd1d7f941f006120753b5ab5af869c
abdo <abdoraja2005@gmail.com>
2023-04-17 22:04:40 +0000

    task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,18 @@
 #ifndef _DOG_H
 #define _DOG_H
 
-struct dog 
+/**
+* struct dog - structure that store info of dog
+* @name: name of the dog
+* @age: the age of the dog
+* @owner: the owner of the dog
+*/
+
+struct dog
 {
 	char *name;
 	float age;
-	char * owner;
+	char *owner;
 };
 
 #endif
