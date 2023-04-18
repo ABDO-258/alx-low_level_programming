d23be4fd693eaa737d03e1d9db8722b659dd51e9
abdo <abdoraja2005@gmail.com>
2023-04-18 01:45:19 +0000

    fix betty add if d is null

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,6 +1,6 @@
 #include "dog.h"
 /**
-*int_dog - initialze the struct dog
+*init_dog - initialze the struct dog
 *@owner: owner of the dog
 *@age:age of the dog
 *@name:name of the dog
@@ -8,11 +8,13 @@
 */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = name;
+	if (d != NULL)
+	{
+		d->name = name;
 	/*(*d).name = name;*/
-	d->age = age;
+		d->age = age;
 	/*(*d).age = age; */
-	d->owner = owner;
+		d->owner = owner;
 	/*(*d).owner = owner;*/
-
+	}
 }
