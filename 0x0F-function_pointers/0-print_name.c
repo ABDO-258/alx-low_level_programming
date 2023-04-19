9ca23f57d69083128249cdee9903909ebd5f549b
abdo <abdoraja2005@gmail.com>
2023-04-19 16:16:20 +0000

    task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+/**
+*print_name - fonction to print name
+*
+*@name: name to print
+*
+*@f: pointer to fonction to print name
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+	if ( name != NULL && f != NULL)
+	f(name);
+}
+	
