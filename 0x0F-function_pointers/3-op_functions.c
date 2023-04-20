0079d3285d2a398a127962c1ec4bbe1ad6fe7562
abdo <abdoraja2005@gmail.com>
2023-04-20 00:11:36 +0000

    task 3

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,71 @@
+/**
+ * op_add - adds a andb
+ *
+ * @a: input 1
+ * @b: input 2
+ *
+ * Return: sum of @a and @b
+*/
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+
+/**
+ * op_sub - substract b from a
+ *
+ * @a: input 1
+ * @b: input 2
+ *
+ * Return: deference of a and b
+*/
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - product of a and b
+ *
+ * @a: input 1
+ * @b: input 2
+ *
+ * Return: product of a and b
+*/
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - division of a with b
+ *
+ * @a: input 1
+ * @b: input 2
+ *
+ * Return: result of a/b
+*/
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+
+/**
+ * op_mod - rmainning of a/b
+ *
+ * @a: input 1
+ * @b: input 2
+ *
+ * Return: reminder
+*/
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
