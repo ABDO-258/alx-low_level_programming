a2964db5d23d4326b4af940248f5c4fcc09664e4
abdo <abdoraja2005@gmail.com>
2023-04-27 14:54:53 +0000

    first task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+/**
+*print_list - print a linked list
+*
+*@h:pointer to head of list
+*
+*Return:number of list
+*/
+size_t print_list(const list_t *h)
+{
+	int i;
+
+	if (h == NULL)
+		return (0);
+	for (i = 1; h->next != NULL; i++, h = h->next)
+	{
+		if (h->str == NULL)
+			printf("[0] (nill)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+	}
+	printf("[%d] %s\n", h->len, h->str);
+	return (i);
+}
