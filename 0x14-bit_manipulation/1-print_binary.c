e9ad38e6848fe2e5d21a2d687a7e82c0d6ba41b4
abdo <abdoraja2005@gmail.com>
2023-05-04 18:45:54 +0100

    task 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+*print_binary- print a dicimal input as binary
+*
+*@n:decimal int
+*
+*/
+
+void print_binary(unsigned long int n)
+{
+	int mask = 1, length = 0;
+	unsigned long int m;
+
+	m = n;
+	while (m > 0)
+	{
+		m >>= 1;
+		length++;
+	}
+	length--;
+
+	if (length > 0)
+		mask <<= (length);
+	while (mask > 0)
+	{
+		if ((n & mask) == 0)
+			_putchar('0');
+		else
+			_putchar('1');
+		mask >>= 1;
+	}
+}
