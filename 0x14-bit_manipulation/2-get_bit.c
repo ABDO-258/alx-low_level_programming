b6d5ad919e37efe487813b7fcacc0baf1ee710d3
abdo <abdoraja2005@gmail.com>
2023-05-04 22:01:19 +0100

    task 2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -2,33 +2,41 @@
 #include "main.h"
 
 /**
-*print_binary- print a dicimal input as binary
+*get_bit- get a bit from a binary number
 *
 *@n:decimal int
+*@index: location of the bit
 *
+*Return: bit or -1 
 */
 
-void print_binary(unsigned long int n)
+int get_bit(unsigned long int n, unsigned int index)
+
 {
-	int mask = 1, length = 0;
-	unsigned long int m;
+    int mask = 1;
+    unsigned long int m, length = 0;
 
-	m = n;
-	while (m > 0)
-	{
-		m >>= 1;
-		length++;
-	}
-	length--;
+    m = n;
+    while (m > 0)
+    {
+        m >>= 1;
+        length++;
+    }
+    length--;
 
-	if (length > 0)
-		mask <<= (length);
-	while (mask > 0)
-	{
-		if ((n & mask) == 0)
-			_putchar('0');
-		else
-			_putchar('1');
-		mask >>= 1;
-	}
+    if (length > 0)
+        mask <<= (length);
+    while (mask > 0)
+    {
+        if (length == index)
+        {
+          if ((n & mask) == 0)
+            return(0);
+        else
+            return(1);
+        }   
+    mask >>= 1;
+    length--;
+    }
+    return (-1);
 }
