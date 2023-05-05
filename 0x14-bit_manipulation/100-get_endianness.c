ee29d1695b1e21cab9743d6f2848d0e46ff46ced
abdo <abdoraja2005@gmail.com>
2023-05-05 14:54:38 +0100

    task 6

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,17 @@
+int get_endianness(void)
+{
+        int num1 = 1, num2 = 1;
+        int count = 0;
+
+        while (num1 != 0)
+        {
+                num1 >>= 1;
+                num2 >>= 1;
+                count++;
+        }
+        if (count != 1)
+                return (0);
+        else
+                return (1);
+
+}
