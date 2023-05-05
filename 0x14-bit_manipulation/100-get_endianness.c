befa35cdf343a25d014a90bd60a857d2ea6607a7
abdo <abdoraja2005@gmail.com>
2023-05-05 15:05:46 +0100

    task 6

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,17 +1,24 @@
+/**
+*get_endianness - define byte ordre
+*
+*Return: 0 if big endian, 1 if little endian
+*/
+
+
 int get_endianness(void)
 {
-        int num1 = 1, num2 = 1;
-        int count = 0;
+	int num1 = 1, num2 = 1;
+	int count = 0;
 
-        while (num1 != 0)
-        {
-                num1 <<= 1;
-                num2 <<= 1;
-                count++;
-        }
-        if (count != 1)
-                return (1);
-        else
-                return (0);
+	while (num1 != 0)
+	{
+		num1 <<= 1;
+		num2 <<= 1;
+		count++;
+	}
+	if (count != 1)
+		return (1);
+	else
+		return (0);
 
 }
