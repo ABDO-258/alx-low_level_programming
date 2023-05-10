cb00dce5a2493af89b2a24bd4e0e5952dff05b7b
abdo <abdoraja2005@gmail.com>
2023-05-11 00:04:10 +0100

    task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,6 +1,38 @@
 #include "main.h"
 #include <string.h>
 /**
+*error_handler- function that print error msg
+*
+*@error: exit error
+*@arg: name of file 1 or file 2
+*@value_of_fd: value of file descriptor
+*
+*Return: 0
+*/
+
+int error_handler(int error, char *arg, int value_of_fd)
+{
+	switch (error)
+	{
+		case 97:
+			dprintf(2, "Usage: cp file_from file_to\n");
+			exit(error);
+		case 98:
+			dprintf(2, "Error: Can't read from file %s\n", arg);
+			exit(error);
+
+		case 99:
+			dprintf(2, "Error: Can't write to %s\n", arg);
+			exit(error);
+		case 100:
+			dprintf(2, "Error: Can't close fd %d\n", value_of_fd);
+			exit(100);
+		default:
+			return (0);
+
+	}
+}
+/**
 *main- function that copy file 1 to file 2
 *
 *@ac: argument counter
@@ -15,48 +47,39 @@ int main(int ac, char **av)
 
 	if (ac != 3)
 	{
-		dprintf(2, "Usage: cp file_from file_to\n");
-		exit(97);
+		error_handler(97, NULL, 0);
 	}
 	op_from = open(av[1], O_RDONLY);
 	if (op_from == -1)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", av[1]);
-		exit(98);
+		error_handler(98, av[1], 0);
 	}
 	op_to = open(av[2],  O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (op_to == -1)
-		{
-			dprintf(2, "Error: Can't write to %s\n", av[2]);
-			exit(99);
-		}
+	{
+		error_handler(99, av[2], 0);
+	}
 	while ((reading = read(op_from, buffer, 1024)) > 0)
 	{
+		if (reading == -1)
+		{
+			error_handler(98, av[1], 0);
+		}
 		writing = write(op_to, buffer, reading);
 		if (writing == -1)
 		{
-			dprintf(2, "Error: Can't write to %s\n", av[2]);
-			exit(99);
+			error_handler(99, av[2], 0);
 		}
 	}
-	/*dprintf(1, "reading value %d\n", reading);
-	dprintf(1, "writing value %d\n", writing);*/
 	closing = close(op_from);
 	if (closing == -1)
 	{
-		dprintf(2, "Error: Can't close fd %d\n", op_from);
-		exit(100);
+		error_handler(100, NULL, op_from);
 	}
 	closing = close(op_to);
 	if (closing == -1)
 	{
-		dprintf(2, "Error: Can't close fd %d\n", op_to);
-		exit(100);
+		error_handler(100, NULL, op_to);
 	}
 	return (0);
 }
-
-
-
-
-
