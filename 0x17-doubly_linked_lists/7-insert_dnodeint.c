e17e6e402412fcfc53c04a1971511f92298337f9
abdo <abdoraja2005@gmail.com>
2023-08-17 18:13:37 +0100

    task [7]try to fix checker error by add return statement to some casse

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -21,7 +21,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (idx == 0)
 	{
-		add_dnodeint(&(*h), n);
+		return (add_dnodeint(&(*h), n));
+
 	}
 	while (current != NULL)
 	{
@@ -45,7 +46,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (count == idx)
 	{
 		/*printf("count is %d\n",count);*/
-		add_dnodeint_end(&(*h), n);
+		return(add_dnodeint_end(&(*h), n));
 	}
 	return (NULL);
 }
